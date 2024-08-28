ea01bb38c5ec8cd43827f2b5face55127dd66bbb
FayedRCS <fayed_rana@outlook.com>
2024-08-28 22:08:01 +0200

    improved and finished v. for main.py

--- main.py
+++ main.py
@@ -1,41 +1,66 @@
 def main():
-
-    with open("books/frankenstein.txt") as f:
-        file_content = f.read()
-
-    words = wordCount(file_content)
-    print("--- Begin report of books/frankenstein.txt ---")
-    print(f"{words} words found in the document\n")
-
-    characters = charCount(file_content)
-    print(f"{characters} are the amount of characters")
-
-    
+    path_of_book = "books/frankenstein.txt"
+    book_text = getting_text(path_of_book)
+    words = word_count(book_text)
+    characters = char_count(book_text)
+    sorted_lst = sorting_characters(characters)
 
 
-def wordCount(text):
-    
-    text = text.split()
-    total_words = len(text)
-    
-    return total_words
+    print(f"--- Begin report of {path_of_book} ---")
 
-def charCount(text):
+    print(f"Scan found {words} words in the text\n")
 
-    char_dict = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "u": 0, "w": 0, "y": 0, "x": 0, "z": 0,}
+    for i in sorted_lst:
+        #checking if key value, titled "char" is in the alphabet
+        if not i["char"].isalpha():
+            continue
+
+        print(f"The '{i['char']}' character was found '{i['amount']}' times")
+
+    print("--- End Scan ---")
+
+
+
+#count the amount of words, seperated by whitespace
+def word_count(text):
+    words = text.split()
+    return len(words)
+
+#breaking down further with character count
+def char_count(text):
+
+    char_dict = {}
     text = text.lower()
-
+    
+    #adds all symbols, but wont matter, we will check if key is in alphabeet later
     for i in text:
-
         if i in char_dict:
             char_dict[i] += 1
-
-    list_of_dict = []
-    for i in char_dict:
-        list_of_dict.append({i:char_dict[i]})
+        else: char_dict[i] = 1
 
     return char_dict
 
 
-main()
 
+def sorting_characters(dict):
+    sorted_list_of_dict = []
+    for i in dict:
+        sorted_list_of_dict.append({"char" : i, "amount": dict[i]})
+
+        #we created a function for sorting, and what to sort. then pass it through it later in the .sort() method. Read documentation, if i forget
+
+    sorted_list_of_dict.sort(reverse=True, key=sort_sys)
+    return sorted_list_of_dict
+
+def sort_sys(subj):
+    return subj["amount"]
+
+
+#function for reading and returning the contents in book file
+def getting_text(path):
+
+    with open(path) as f:
+        contents = f.read()
+    return contents
+
+main()
