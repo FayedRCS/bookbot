fee05311b003aa0c4c56b79415fe94ce222d681c
fayed <fayed_rana@outlook.com>
2025-05-13 21:50:47 +0200

    added access to CLI args

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from stats import word_count, char_count, sorting_characters
+import sys #provides access to command line arguments
+
+
+def getting_text(path):
+
+    with open(path) as f:
+        contents = f.read()
+        return contents
 
 def main():
 
-    path_of_book = "books/frankenstein.txt"
-
+    path_of_book = sys.argv[1] #['main.py', 'books/frankenstein.txt'], the index 1 is the path to the book
     book = getting_text(path_of_book)
+
     words = word_count(book)
     characters = char_count(book)
     sorted_format = sorting_characters(characters)
@@ -24,12 +32,14 @@ def main():
 
     print("============= END ===============")
 
+
+if len(sys.argv) != 2:
+        print('Usage: python3 main.py <path_to_book>')
+        (sys.exit(1))
+
+else:
+    main()
+
     
 
-def getting_text(path):
 
-    with open(path) as f:
-        contents = f.read()
-    return contents
-
-main()
